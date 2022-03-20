3daa1e37b1c562a18c6392bf9450b79103ce3413
arkivm <vikram186@gmail.com>
2022-03-20 01:42:04 -0700

    Create profile for ksplit

---
+++ ksplit-setup.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+MOUNT_DIR=/mnt/ksplit
+
+# Install llvm-10 from apt.llvm.org
+install_llvm() {
+  wget https://apt.llvm.org/llvm.sh
+  chmod +x llvm.sh
+  sudo ./llvm.sh 10
+  # TODO: Setup update-alternatives for clang
+}
+
+install_dependencies() {
+  sudo apt update
+  sudo apt install -y build-essential
+  install_llvm
+}
+
+prepare_local_partition() {
+  GROUP=$(getent group  | grep ${SUDO_GID} | cut -d':' -f1)
+  sudo mkfs.ext4 /dev/sda4
+  sudo mkdir ${MOUNT_DIR}
+  sudo mount -t ext4 /dev/sda4 ${MOUNT_DIR}
+  sudo chown -R ${SUDO_USER}:${GROUP} ${MOUNT_DIR}
+}
+
+prepare_machine() {
+  install_dependencies
+  prepare_local_partition
+}
+
+
+# Clone all repos
+clone_pdg() {
+  pushd ${MOUNT_DIR}
+  git clone https://github.com/ARISTODE/program-dependence-graph.git pdg
+  popd;
+}
+
+clone_linux() {
+  pushd ${MOUNT_DIR}
+  git clone https://github.com/mars-research/lvd-linux/ --branch dev_vmfunc --depth 500
+  popd;
+}
+
+clone_repos() {
+  clone_pdg;
+  clone_linux;
+}
+
+## Build
+build_pdg() {
+  pushd ${MOUNT_DIR}/pdg
+  mkdir build && cd build;
+  cmake .. && make -j $(nproc)
+}
+
+build_linux() {
+  pushd ${MOUNT_DIR}/lvd-linux;
+  cp config_lvd .config
+  make -j $(nproc)
+}
+
+build_all() {
+  build_pdg;
+  build_linux;
+}
+
+prepare_machine;
+clone_repos;
+build_all;
--- profile.py
+++ profile.py
@@ -1,10 +1,4 @@
-"""This is a trivial example of a gitrepo-based profile; The profile source code and other software, documentation, etc. are stored in in a publicly accessible GIT repository (say, github.com). When you instantiate this profile, the repository is cloned to all of the nodes in your experiment, to `/local/repository`. 
-
-This particular profile is a simple example of using a single raw PC. It can be instantiated on any cluster; the node will boot the default operating system, which is typically a recent version of Ubuntu.
-
-Instructions:
-Wait for the profile instance to start, then click on the node in the topology and choose the `shell` menu item. 
-"""
+"""KSplit evaluation"""
 
 # Import the Portal object.
 import geni.portal as portal
@@ -16,12 +10,13 @@ pc = portal.Context()
 
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
- 
-# Add a raw PC to the request.
-node = request.RawPC("node")
+
+node_0 = request.RawPC('node-0')
+node_0.hardware_type = 'd820'
+node_0.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
 
 # Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
+node_0.addService(pg.Execute(shell="sh", command="/local/repository/ksplit-setup.sh"))
 
-# Print the RSpec to the enclosing page.
+# Print the generated rspec
 pc.printRequestRSpec(request)
