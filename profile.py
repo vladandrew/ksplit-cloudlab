cf75d3cfa3e1de0b4adb2a0935bcb5b9d134a4fe
Vikram Narayanan <vikram186@gmail.com>
2022-04-13 21:44:23 -0600

    Switch to the latest image

--- profile.py
+++ profile.py
@@ -13,8 +13,7 @@ request = pc.makeRequestRSpec()
 
 node_0 = request.RawPC('node-0')
 node_0.hardware_type = 'c220g2'
-node_0.disk_image = 'urn:publicid:IDN+wisc.cloudlab.us+image+xcap-PG0:ksplit-eval-220g2'
-#node_0.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
+node_0.disk_image = 'urn:publicid:IDN+wisc.cloudlab.us+image+redshift-PG0:ksplit-eval-test-nd'
 
 # Install and execute a script that is contained in the repository.
 node_0.addService(pg.Execute(shell="sh", command="/local/repository/ksplit-top.sh"))
